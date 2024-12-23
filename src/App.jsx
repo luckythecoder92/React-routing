1e1a286549817696cf67729973ed30b5946d4cba
luckythe coder92 <www.sonilakshya351@gmail.com>
2024-12-23 20:45:15 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import About from './pages/About'
+import Home from './pages/Home'
+import Contact from './pages/Contact'
+import Product from './pages/Product'
+import Header from './components/Header'
+
+const App = () => {
+  return (
+    <div>
+      <Header/>
+      <Routes>
+        <Route path='/' element = {<Home/>}/>
+        <Route path='/about' element = {<About/>}/>
+        <Route path='/contact' element = {<Contact/>}/>
+        <Route path='/product' element = {<Product/>}/>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
